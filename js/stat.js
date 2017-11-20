ee675bfa6ce6a0b16ea5c16b5554eef81c834d9c
AnyaAnandi <m-anandi@mail.ru>
2017-11-20 20:05:33 +0300

    Вроде разобралась

--- js/stat.js
+++ js/stat.js
@@ -1,15 +1,63 @@
 'use strict';
 window.renderStatistics = function (ctx, names, times) {
 
+  /* ctx.shadowOffsetX = 10;
+  ctx.shadowOffsetY = 10;
+  ctx.shadowColor = 'rgba(0, 0, 0, 0.7)'; */
+
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.beginPath();
+  ctx.moveTo(110, 20);
+  ctx.lineTo(150, 40);
+  ctx.lineTo(190, 20);
+  ctx.lineTo(230, 40);
+  ctx.lineTo(270, 20);
+  ctx.lineTo(310, 40);
+  ctx.lineTo(350, 20);
+  ctx.lineTo(390, 40);
+  ctx.lineTo(430, 20);
+  ctx.lineTo(490, 30);
+  ctx.lineTo(530, 20);
+
+  ctx.lineTo(510, 46);
+  ctx.lineTo(530, 72);
+  ctx.lineTo(510, 98);
+  ctx.lineTo(530, 124);
+  ctx.lineTo(510, 150);
+  ctx.lineTo(530, 176);
+  ctx.lineTo(510, 202);
+  ctx.lineTo(530, 228);
+  ctx.lineTo(510, 254);
+  ctx.lineTo(530, 290);
+
+  ctx.lineTo(490, 270);
+  ctx.lineTo(440, 290);
+  ctx.lineTo(390, 270);
+  ctx.lineTo(350, 290);
+  ctx.lineTo(310, 270);
+  ctx.lineTo(270, 290);
+  ctx.lineTo(230, 270);
+  ctx.lineTo(190, 290);
+  ctx.lineTo(150, 270);
+  ctx.lineTo(110, 290);
+
+  ctx.lineTo(130, 254);
+  ctx.lineTo(110, 228);
+  ctx.lineTo(130, 202);
+  ctx.lineTo(110, 176);
+  ctx.lineTo(130, 150);
+  ctx.lineTo(110, 124);
+  ctx.lineTo(130, 98);
+  ctx.lineTo(110, 72);
+  ctx.lineTo(130, 46);
+  ctx.closePath();
+  ctx.fill();
+
   var gradient = ctx.createLinearGradient(100, 10, 420, 270);
   gradient.addColorStop(0, 'green');
   gradient.addColorStop(1, 'white');
 
   ctx.fillStyle = gradient;
-  ctx.shadowOffsetX = 10;
-  ctx.shadowOffsetY = 10;
-  ctx.shadowColor = 'rgba(0, 0, 0, 0.7)';
-  ctx.shadowBlur = 10;
   ctx.strokeStyle = 'green';
   ctx.lineWidth = 5;
 
@@ -61,18 +109,16 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.stroke();
   ctx.fill();
 
-
-  // как сделать тень только на мою фигуру???
+  /* ctx.shadowOffsetX = 0;
+  ctx.shadowOffsetY = 0;
+  ctx.shadowColor = 'transparent'; */
 
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
   ctx.fillText('Ура вы победили!', 135, 45);
   ctx.fillText('Список результатов:', 135, 65);
 
-  // если выношу функцию в самый верх кода, до window. ...., то она не работает
-  // и почему она сразу не работает если пишу function(times) ????
-
-  var findMaxTime = function (arr) {
+  function findMaxTime(arr) {
     var maxTime = 0;
     /* var maxTimeIndex = 0; */
     for (var i = 0; i < arr.length; i++) {
@@ -83,9 +129,7 @@ window.renderStatistics = function (ctx, names, times) {
       }
     }
     return maxTime;
-  };
-  findMaxTime(times);
-  /* console.log(times); */
+  }
 
   var histogramHeight = 150;
   var step = histogramHeight / (findMaxTime(times) - 0);
@@ -96,36 +140,41 @@ window.renderStatistics = function (ctx, names, times) {
   var lineHeight = 20;
   var value = 'Вы';
 
+  for (var j = 0; j < times.length && j < names.length; j++) {
 
-  for (var j = 0; j < times.length; j++) {
+    if (names[j] === value) {
+
+      ctx.fillStyle = 'rgb(255, 0, 0)';
+      ctx.globalAlpha = 1;
+    } else {
+      ctx.fillStyle = 'rgb(0, 0, 250)';
+      ctx.globalAlpha = Math.random();
+    }
+
     ctx.fillRect(startX + margin * j, startY, barWidth, -times[j] * step);
     ctx.fillText(names[j], startX + margin * j, startY + lineHeight);
     ctx.fillText(Math.round(times[j]), startX + margin * j, startY - histogramHeight - lineHeight / 2);
 
   }
-  /* console.log(names); */
 
-  // ниже код не красит, я всю голову сломала - по дебагеру все работает
-  // а по факту не красит нужный прямоугольник
+  /* Функции вычисляющие рандомные знанчения
 
-  /* var randColor = function () {
-    var r = Math.floor(Math.random() * (256)),
-      g = Math.floor(Math.random() * (256)),
+  function getRandomInt(min, max) {
+
+    return Math.random() * (max - min + 1) + min;
+
+  }
+  console.log(getRandomInt(0, 1).toFixed(2));
+
+
+   var randColor = function () {
+    var r = 0,
+      g = 0,
       b = Math.floor(Math.random() * (256));
     return '#' + r.toString(16) + g.toString(16) + b.toString(16);
   }
    console.log(randColor()); */
 
-  // как сделать случайную прозрачность?
-
-  for (var i = 0; i < names.length; i++) {
-    if (names[i] === value) {
-
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      ctx.fillStyle = 'randColor';
-    }
-  }
 };
 
 
