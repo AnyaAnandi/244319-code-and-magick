429baab3f92b0516585df5509ea919563d02f2fa
AnyaAnandi <m-anandi@mail.ru>
2017-11-24 17:54:57 +0300

    sort круто работает

--- js/setup.js
+++ js/setup.js
@@ -4,26 +4,24 @@ var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Крис�
 var surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
-
-var randomNames = [];
-var randomSurnames = [];
-var randomEyes = [];
-var randomCoats = [];
 var allWizards = [];
 
-function getRandom(arr, arr1) {
-  for (var i = 0; i < size; i++) {
-    var rand = Math.floor(Math.random() * arr.length);
-    arr1[i] = arr[rand];
-  }
-  return arr1;
+function shuffleArray(arr) {
+  arr.sort(function () {
+    return 0.5 - Math.random();
+  });
 }
+
+shuffleArray(names);
+shuffleArray(surnames);
+shuffleArray(coatColor);
+shuffleArray(eyesColor);
 
 for (var i = 0; i < size; i++) {
   allWizards[i] = {
-    namesurname: getRandom(names, randomNames)[i] + ' ' + getRandom(surnames, randomSurnames)[i],
-    coat: getRandom(coatColor, randomCoats)[i],
-    eye: getRandom(eyesColor, randomEyes)[i]
+    name: names[i] + ' ' + surnames[i],
+    coatColor: coatColor[i],
+    eyesColor: eyesColor[i]
   };
 }
 
@@ -32,13 +30,13 @@ var similarList = document.querySelector('.setup-similar-list');
 
 var wizardTemplate = document.querySelector('#similar-wizard-template').content;
 
-function renderWizard(arr) {
+function renderWizard(wizard) {
   for (var l = 0; l < size; l++) {
     var wizardOne = wizardTemplate.cloneNode(true);
 
-    wizardOne.querySelector('.setup-similar-label').textContent = arr.namesurname;
-    wizardOne.querySelector('.wizard-coat').style.fill = arr.coat;
-    wizardOne.querySelector('.wizard-eyes').style.fill = arr.eye;
+    wizardOne.querySelector('.setup-similar-label').textContent = wizard.name;
+    wizardOne.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+    wizardOne.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
   }
   return wizardOne;
 }
